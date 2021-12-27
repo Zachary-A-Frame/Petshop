b6fa35d4be772f2919a2ddc9bd9eb192c8ab73de
Zach <Zacaframe@gmail.com>
2021-12-26 23:24:05 -0500

    Finished

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template, flash, redirect
+from flask import Flask, url_for, render_template, flash, redirect, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
 # from models import db, connect_db, User, Post, Tag, PostTag
 from password import password
 from models import db, connect_db, Pet
-from forms import AddPetForm
+from forms import AddPetForm, EditPetForm
 
 app = Flask(__name__)
+
 
 app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://postgres:{password}@localhost:5432/petshop"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -49,3 +50,33 @@ def add_pet():
 
     else:
         return render_template("pet_add_form.html", form=form)
+
+
+@app.route("/<int:pet_id>", methods=["GET", "POST"])
+def edit_pet(pet_id):
+    """Edit pet."""
+
+    pet = Pet.query.get_or_404(pet_id)
+    form = EditPetForm(obj=pet)
+
+    if form.validate_on_submit():
+        pet.notes = form.notes.data
+        pet.available = form.available.data
+        pet.photo_url = form.photo_url.data
+        db.session.commit()
+        flash(f"{pet.name} updated.")
+        return redirect(url_for('list_pets'))
+
+    else:
+        # failed; re-present form for editing
+        return render_template("pet_edit_form.html", form=form, pet=pet)
+
+
+@app.route("/api/pets/<int:pet_id>", methods=['GET'])
+def api_get_pet(pet_id):
+    """Return basic info about pet in JSON."""
+
+    pet = Pet.query.get_or_404(pet_id)
+    info = {"name": pet.name, "age": pet.age}
+
+    return jsonify(info)
--- forms.py
+++ forms.py
@@ -1,12 +1,24 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, TextAreaField
-
+from wtforms import StringField, IntegerField, SelectField, TextAreaField, BooleanField
+from wtforms.validators import InputRequired, Length, NumberRange, URL, Optional
 class AddPetForm(FlaskForm):
     """Form for adding pets."""
 
-    name = StringField("Pet Name")
-    species = StringField("Species")
-    photo_url = StringField("Photo url (Optional)")
-    age = IntegerField("Age")
-    notes = TextAreaField("Note (Optional)")
+    name = StringField("Pet Name",
+                       validators=[InputRequired()])
+    species = SelectField("Species",
+                          choices=[("cat", "Cat"), ("dog", "Dog"), ("porcupine", "Porcupine")])
+    photo_url = StringField("Photo url (Optional)",
+                            validators=[Optional(), URL()])
+    age = IntegerField("Age", validators=[Optional(), NumberRange(min=0, max=30)])
+    notes = TextAreaField("Note (Optional)", validators=[Optional(), Length(min=10)])
+
+
+class EditPetForm(FlaskForm):
+    """Editing an existing pet."""
+    photo_url = StringField("Poto URL",
+                            validators=[Optional(), URL()])
+    notes = TextAreaField("Comments",
+                          validators=[Optional(), Length(min=10)])
+    available = BooleanField("Available?")
 
